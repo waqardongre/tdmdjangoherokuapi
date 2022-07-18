954ecd08efe6a68961134ecf6d3c16e38e045ddd
waqardongre <waqardongre@gmail.com>
2022-07-18 17:07:17 +0530

    added 'django.contrib.admindocs' in settings.py

--- tdm/settings.py
+++ tdm/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
