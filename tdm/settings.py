3d54c6568023dc626a91cefbb0baec8e3d52b173
waqardongre <waqardongre@gmail.com>
2022-07-18 19:55:33 +0530

    Base dir val changed in settings.py

--- tdm/settings.py
+++ tdm/settings.py
@@ -3,8 +3,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(__file__).replace('\\','/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
