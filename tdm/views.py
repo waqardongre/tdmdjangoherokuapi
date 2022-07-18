480e661b643fd7391ff3cb3d0c53475a21be3969
waqardongre <waqardongre@gmail.com>
2022-07-18 13:48:51 +0530

    added again rest_framework from apps list in settings.py

--- tdm/views.py
+++ tdm/views.py
@@ -1,6 +1,5 @@
-#from django.shortcuts import render
-from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import FileSerializer
