85c980a2c6951151804511996231a6e85d70d11f
waqardongre <waqardongre@gmail.com>
2022-07-18 09:13:30 +0530

    Remove a hardcoded Django secret key

--- tdm/settings.py
+++ tdm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-do-0hw@z@d*o_z6h-69s#5kck8n)4bm7xtt_ob4fcmp(qkfldz'
+#SECRET_KEY = 'django-insecure-do-0hw@z@d*o_z6h-69s#5kck8n)4bm7xtt_ob4fcmp(qkfldz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
