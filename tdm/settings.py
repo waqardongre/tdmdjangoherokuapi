7cbc1bd738919b5c0e453d3f952dad031a7f53f4
waqardongre <waqardongre@gmail.com>
2022-07-18 19:34:26 +0530

    Base dir val changed in database NAME property settings.py

--- tdm/settings.py
+++ tdm/settings.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+#from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'tdm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
