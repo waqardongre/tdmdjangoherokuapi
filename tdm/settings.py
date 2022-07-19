a4cffeacf76734fb8bc92c5cc383e2646c85fae3
waqardongre <waqardongre@gmail.com>
2022-07-19 18:15:29 +0530

    updated glitch server url for production

--- tdm/settings.py
+++ tdm/settings.py
@@ -127,7 +127,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://wonderful-boundless-pastry.glitch.me"
+    "https://broad-ethereal-actor.glitch.me"
 ]
 
 CORS_ALLOW_METHODS = [
