2d8c7a3090f03612e88b6efe17eda95c2be39853
waqardongre <waqardongre@gmail.com>
2022-07-19 21:23:36 +0530

    updated glitch server url for production

--- tdm/settings.py
+++ tdm/settings.py
@@ -127,7 +127,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://skillful-stingy-wish.glitch.me"
+    "https://skillful-stingy-wish.glitch.me",
 ]
 
 CORS_ALLOW_METHODS = [
